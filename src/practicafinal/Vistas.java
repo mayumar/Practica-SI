5084f65e3b4a25255801536fe88c20e68979e545
mayumar <maytalbada@gmail.com>
2024-05-08 18:32:44 +0200

    Funciones de vistas

--- src/practicafinal/Vistas.java
+++ src/practicafinal/Vistas.java
@@ -8,14 +8,26 @@ import javax.swing.*;
 public class Vistas {
     
     public static ArrayList<JPanel> create_vistas(){
-        //Colores
-        Color RisingBlack = new Color(27, 28, 34); //Negro
 
         ArrayList<JPanel> views = new ArrayList<JPanel>();
 
         FlowLayout fl = new FlowLayout();
         fl.setAlignment(FlowLayout.CENTER);
 
+
+        views.add(createInicio());
+        views.add(createJuegos());
+        views.add(createCategorias());
+        views.add(createListaCategoria());
+        views.add(createJuego());
+        views.add(createReview());
+
+        return views;
+    }
+
+    public static JPanel createInicio(){
+        //Colores
+        Color RisingBlack = new Color(27, 28, 34); //Negro
 
         JPanel inicio = new JPanel(new GridLayout(10,1));
         inicio.add(new JLabel("INICIO"));
@@ -24,31 +36,58 @@ public class Vistas {
         separator.setBackground(RisingBlack);
         inicio.add(separator);
         inicio.add(new JLabel("HOLAAAA"));
-        views.add(inicio);
+
+        return inicio;
+    }
+
+    public static JPanel createJuegos(){
+        FlowLayout fl = new FlowLayout();
+        fl.setAlignment(FlowLayout.CENTER);
 
         JPanel juegos = new JPanel(fl);
         juegos.add(new JLabel("JUEGOS"));
-        views.add(juegos);
+
+        return juegos;
+    }
+
+    public static JPanel createCategorias(){
+        FlowLayout fl = new FlowLayout();
+        fl.setAlignment(FlowLayout.CENTER);
 
         JPanel categorias = new JPanel(fl);
         categorias.add(new JLabel("CATEGORIAS"));
-        views.add(categorias);
 
-        JPanel categoria1 = new JPanel(fl);
-        categoria1.add(new JLabel("FPS"));
-        views.add(categoria1);
+        return categorias;
+    }
+
+    public static JPanel createListaCategoria(){
+        FlowLayout fl = new FlowLayout();
+        fl.setAlignment(FlowLayout.CENTER);
+
+        JPanel categoria = new JPanel(fl);
+        categoria.add(new JLabel("FPS"));
+
+        return categoria;
+    }
+
+    public static JPanel createJuego(){
+        FlowLayout fl = new FlowLayout();
+        fl.setAlignment(FlowLayout.CENTER);
 
         JPanel juego = new JPanel(fl);
         juego.add(new JLabel("JUEGO"));
-        views.add(juego);
+
+        return juego;
+    }
+
+    public static JPanel createReview(){
+        FlowLayout fl = new FlowLayout();
+        fl.setAlignment(FlowLayout.CENTER);
 
         JPanel review = new JPanel(fl);
         review.add(new JLabel("REVIEW"));
-        views.add(review);
 
-        return views;
+        return review;
     }
-
-    
 
 }
