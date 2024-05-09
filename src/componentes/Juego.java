62faffff557285bc4e77a8497b88f4f3504c3346
Pacatro <paco.algar.15@gmail.com>
2024-05-09 12:22:22 +0200

    error con el borde del jueguito

--- src/componentes/Juego.java
+++ src/componentes/Juego.java
@@ -15,7 +15,6 @@ public class Juego {
 
     public JPanel crear() {
         Color RisingBlack = new Color(27, 28, 34); //Negro
-        Color White = new Color(255, 255, 255); //Negro
 
         //Bordes
         Border black_border = BorderFactory.createLineBorder(RisingBlack, 2);
@@ -23,7 +22,6 @@ public class Juego {
         ImageIcon imagen = new ImageIcon("src/images/juegos/fps/p_cs2.png");
         
         JPanel panel_juego = new JPanel(new BorderLayout());
-        panel_juego.setBackground(White);
         panel_juego.setBorder(black_border);
         panel_juego.add(new JLabel(imagen));
 
--- src/practicafinal/paginas/Juegos.java
+++ src/practicafinal/paginas/Juegos.java
@@ -8,12 +8,19 @@ import java.awt.*;
 
 public class Juegos {
     public static JPanel createJuegos(String nlista){
-        FlowLayout fl = new FlowLayout();
-        fl.setAlignment(FlowLayout.CENTER);
+        Color RisingBlack = new Color(27, 28, 34); //Negro
 
-        JPanel juegos = new JPanel(fl);
-        juegos.add(new JLabel(nlista));
+        JPanel juegos = new JPanel(new BorderLayout());
         juegos.add(new Juego("cs2").crear());
+
+        JLabel titulo = new JLabel(nlista);
+        titulo.setForeground(RisingBlack);
+        titulo.setBackground(RisingBlack);
+
+        juegos.add(titulo, BorderLayout.NORTH);
+
+        juegos.add(new Linea(0, 10, 2000, 10), BorderLayout.CENTER);
+        juegos.add(new Juego("CS2").crear(), BorderLayout.SOUTH);
 
         return juegos;
     }
