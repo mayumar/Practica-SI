40510632274e55abc51d1c081a608d064ffaf7d3
mayumar <maytalbada@gmail.com>
2024-05-11 16:29:10 +0200

    Descatados ma o meno

--- src/practicafinal/paginas/Inicio.java
+++ src/practicafinal/paginas/Inicio.java
@@ -25,15 +25,15 @@ public class Inicio extends JPanel{
         // Crear el carrousel
         Carrousel carrousel = new Carrousel(elements, 3);
 
+        JPanel contenido = new JPanel(new BorderLayout());
+        
         // Añadir el carrousel al panel
-        JPanel contenido = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.BOTH;
-
-        contenido.add(carrousel);
+        contenido.add(carrousel, BorderLayout.NORTH);
         
         //Titulo de destacados
-        contenido.add(new Titulo("DESTACADOS", false));
+        contenido.add(new Titulo("DESTACADOS", false), BorderLayout.CENTER);
+
+        contenido.add(new JLabel("JUEGO 1"), BorderLayout.SOUTH);
 
         add(contenido, BorderLayout.CENTER);
     }
