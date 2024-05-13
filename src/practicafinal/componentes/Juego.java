90764300912cc6714c3fcbaeb531bc8fa994f3ff
Pacatro <paco.algar.15@gmail.com>
2024-05-13 12:21:29 +0200

    componente jeugo arreglado

--- src/practicafinal/componentes/Juego.java
+++ src/practicafinal/componentes/Juego.java
@@ -5,25 +5,15 @@ import javax.swing.border.Border;
 
 import java.awt.*;
 
-public class Juego {
+public class Juego extends JButton {
     private String nombre;
 
     public Juego(String nombre) {
-        this.nombre = nombre; 
-    }
-
-    public JPanel crear() {
-        Color RisingBlack = new Color(27, 28, 34); //Negro
-
-        //Bordes
-        Border black_border = BorderFactory.createLineBorder(RisingBlack, 2);
-        
+        this.nombre = nombre;
         ImageIcon imagen = new ImageIcon("src/images/juegos/fps/p_cs2.png");
-        JPanel panel_juego = new JPanel(new BorderLayout());
-        panel_juego.setSize(new Dimension(207, 224));
-        panel_juego.setBorder(black_border);
-        panel_juego.add(new JLabel(imagen));
-
-        return panel_juego;
+        setIcon(imagen);
+        setPreferredSize(new Dimension(207, 224));
+        setBorder(Bordes.Black_border);
+        setBackground(Colores.CadetGray);
     }
 }
