6a749844e7f8df07333b07df38561fbd49c926e6
Pacatro <paco.algar.15@gmail.com>
2024-05-19 13:26:19 +0200

    añadiendo nuevos javadocs

--- src/practicafinal/paginas/PortadaJuego.java
+++ src/practicafinal/paginas/PortadaJuego.java
@@ -6,8 +6,16 @@ import practicafinal.componentes.Titulo;
 
 import java.awt.*;
 
+/**
+ * La clase PortadaJuego representa un panel que muestra la portada de un juego específico.
+*/
 public class PortadaJuego extends JPanel{
-
+    /**
+     * Crea una instancia de PortadaJuego. Este panel utiliza un BorderLayout y contiene un título en la parte superior
+     * que muestra el nombre del juego.
+     *
+     * @param gameName El nombre del juego que se mostrará en la portada.
+    */
     public PortadaJuego(String gameName){
         setLayout(new BorderLayout());
         add(new Titulo(gameName, false), BorderLayout.NORTH);
--- src/practicafinal/paginas/Review.java
+++ src/practicafinal/paginas/Review.java
@@ -6,8 +6,13 @@ import practicafinal.componentes.Titulo;
 
 import java.awt.*;
 
+/**
+ * La clase Review representa un panel en el que los usuarios pueden dejar sus reseñas.
+*/
 public class Review extends JPanel{
-
+    /**
+     * Crea una instancia de Review. Este panel utiliza un BorderLayout y contiene un título en la parte superior.
+    */
     public Review(){
         setLayout(new BorderLayout());
         add(new Titulo("Deja tu reseña", false), BorderLayout.NORTH);
